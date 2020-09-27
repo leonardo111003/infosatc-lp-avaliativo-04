be34082e2ade30c9e38a5994f74002040832ab78
leonardo111003 <leocatra123@hotmail.com>
2020-09-27 13:15:00 -0300

    feat : adiciona exercicio3

---
+++ exercicio3.py
@@ -0,0 +1,6 @@
+palavra = input("digite qualquer palavra")
+def parametroInvertido (palavra1):
+    palavraaocontrario = palavra1 [::-1]
+    print( palavraaocontrario)
+    parametroInvertido (palavra)
+
