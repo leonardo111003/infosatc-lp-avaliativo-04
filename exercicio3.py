9926f5d8e40104c1cb1473b07b34e6820eb7c61e
leonardo111003 <leocatra123@hotmail.com>
2020-09-27 13:59:34 -0300

    fix : altera texto

---
+++ exercicio3.py
@@ -0,0 +1,5 @@
+palavra= input("digite uma palavra pra ser escrita ao contrario: ")
+def parametroinverso(palavra):
+    palavraaocontrario = palavra[::-1]
+    print(palavraaocontrario)
+parametroinverso(palavra)
